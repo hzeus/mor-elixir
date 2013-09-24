8c7a2f42a66f56acfc86cdb7405f70b362aa6f2b
Heiko Zeus <heiko.zeus@zweitag.de>
2013-09-24 10:07:46 +0200

    Switch completely to doctests

--- sequence.exs
+++ sequence.exs
@@ -3,8 +3,11 @@ defmodule Sequence do
   @doc """
   Maps a collection to another collection applying the given function to each element.
 
-    iex> Sequence.map( [ 1, 2, 3 ], &1+&1)
+    iex> Sequence.map( [ 1, 2, 3 ], &1 * &1)
     [ 1, 4, 9 ]
+
+    iex> Sequence.map( [], &1 * &1)
+    [ ]
   """
   def map([], _fun), do: []
   def map([h|t], fun), do: [ fun.(h) | map(t, fun) ]
@@ -17,13 +20,4 @@ ExUnit.start
 defmodule SequenceTest do
   use ExUnit.Case
   doctest Sequence
-
-  test 'map an empty list' do
-    assert [] |> Sequence.map(&1 * &1) == []
-  end
-
-  test 'map a non-empty list' do
-    #assert Sequence.map( [ 1, 2, 3 ], fn x -> x * x end ) == [ 1, 4, 9 ]
-    assert [ 1, 2, 3 ] |> Sequence.map(&1 * &1) == [ 1, 4, 9 ]
-  end
 end
